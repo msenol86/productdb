ed6e3bcb0d603f4d14e4e11b5e7d84eb3815e6dd
Mucahit Senol <mucahitsenol86@gmail.com>
2016-05-02 18:48:43 +0300

    api.py -> type_api.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, Response, request
-import api
+import type_api
 import typing
 import json
 
@@ -11,7 +11,7 @@ def _json_fail():
 
 
 def _explode_and_fetch(type_label: str, fetch_function: typing.Callable[[typing.Any], typing.Any]) -> Response:
-    result_object = api.explode_type_label(type_label.strip())
+    result_object = type_api.explode_type_label(type_label.strip())
     if result_object:
         fetch_json = fetch_function(**result_object)
         if fetch_json:
@@ -33,24 +33,24 @@ def hello():
 
 @app.route("/get_type/<type_label>")
 def get_type(type_label: str):
-    return _explode_and_fetch(type_label, api.get_type)
+    return _explode_and_fetch(type_label, type_api.get_type)
 
 
 @app.route("/get_children_types/<type_label>")
 def get_children_types(type_label: str):
-    return _explode_and_fetch(type_label, api.get_children_types)
+    return _explode_and_fetch(type_label, type_api.get_children_types)
 
 
 @app.route("/get_parent_type/<type_label>")
 def get_parent_types(type_label: str):
-    return _explode_and_fetch(type_label, api.get_parent_type)
+    return _explode_and_fetch(type_label, type_api.get_parent_type)
 
 
 @app.route("/new_type/<new_type_name>/extends/<parent_type_label>")
 def new_type(new_type_name: str, parent_type_label: str):
-    result_object = api.explode_type_label(parent_type_label)
+    result_object = type_api.explode_type_label(parent_type_label)
     if result_object:
-        fetch_json = api.extend_type(new_type_name, result_object["meta_name"], result_object["meta_version"])
+        fetch_json = type_api.extend_type(new_type_name, result_object["meta_name"], result_object["meta_version"])
         if fetch_json:
             return jsonify({"result": fetch_json})
         else:
@@ -61,13 +61,13 @@ def new_type(new_type_name: str, parent_type_label: str):
 
 @app.route("/add_fields/<type_label>", methods=['POST'])
 def add_fields(type_label: str):
-    result_object = api.explode_type_label(type_label)
+    result_object = type_api.explode_type_label(type_label)
     if result_object:
         new_fields_list = json.loads(request.form["new_fields"])
         if new_fields_list:
-            fetch_json = api.add_fields_to_type(result_object["meta_name"],
-                                                result_object["meta_version"],
-                                                new_fields_list)
+            fetch_json = type_api.add_fields_to_type(result_object["meta_name"],
+                                                     result_object["meta_version"],
+                                                     new_fields_list)
             if fetch_json:
                 return jsonify({"result": fetch_json})
             else:
